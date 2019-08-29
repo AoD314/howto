e4e590eda82e6cc665615bcbdf6010dbfb1af53e
Andrey Morozov <morozov.andrey.vmk@gmail.com>
2019-08-29 13:39:43 +0300

    Update gen_benchmark_video.py

--- gen_benchmark_video.py
+++ gen_benchmark_video.py
@@ -1,12 +1,92 @@
 from PIL import Image, ImageDraw, ImageFont
+import random
 
-IMAGE_SIZE = (1920, 1080)
+settings = {
+    'img_size': (1920, 1080),
+    'ratio': 0.2
+}
 
-image = Image.new(mode='RGBA', size=IMAGE_SIZE)
+colors = [
+    (0, 0, 128, 255),
+    (0, 0, 255, 255),
+    (0, 128, 0, 255),
+    (0, 128, 128, 255),
+    (0, 128, 255, 255),
+    (0, 255, 0, 255),
+    (0, 255, 128, 255),
+    (0, 255, 255, 255),
+    (128, 0, 0, 255),
+    (128, 0, 128, 255),
+    (128, 0, 255, 255),
+    (128, 128, 0, 255),
+    (128, 128, 128, 255),
+    (128, 128, 255, 255),
+    (128, 255, 0, 255),
+    (128, 255, 128, 255),
+    (128, 255, 255, 255),
+    (255, 0, 0, 255),
+    (255, 0, 128, 255),
+    (255, 0, 255, 255),
+    (255, 128, 0, 255),
+    (255, 128, 128, 255),
+    (255, 128, 255, 255),
+    (255, 255, 0, 255),
+    (255, 255, 128, 255),
+    (255, 255, 255, 255)
+]
 
-d = ImageDraw.Draw(image)
-fnt = ImageFont.truetype('LiberationMono-Regular.ttf', 36)
-d.text((10, 10), "0000", font=fnt, fill=(255,255,255, 255))
-d.ellipse([200, 200, 400, 400], fill=(255, 0, 0, 255), )
 
-image.save('image.webp', lossless=1, method=6)
+class Ball:
+    def __init__(self, x, y, r, color, border):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.color = color
+        self.border = border
+        self.dir = [
+            ((1 << 15) - random.randint(0, 1<<16)) / float(1 << 15),
+            ((1 << 15) - random.randint(0, 1<<16)) / float(1 << 15)
+            ]
+
+    def iter(self, speed=1):
+        self.x += self.dir[0] * speed
+        self.y += self.dir[1] * speed
+
+        self.dir[0] = -self.dir[0] if self.x <= 0 else self.dir[0]
+        self.dir[1] = -self.dir[1] if self.y <= 0 else self.dir[1]
+
+        self.dir[0] = -self.dir[0] if self.x >= self.border[0] else self.dir[0]
+        self.dir[1] = -self.dir[1] if self.y >= self.border[1] else self.dir[1]
+
+class Balls:
+    def __init__(self, speed=0.1):
+        self.balls = [Ball(100, 100, 100, colors[-8], settings['img_size'])]
+        self.speed = speed * settings['img_size'][1]
+        self.frame_number = 0
+
+    def iter(self):
+        self.frame_number += 1
+        for b in self.balls:
+            b.iter(speed=self.speed)
+
+        return 'image_{:05d}'.format(self.frame_number)
+
+    def render(self, image):
+        d = ImageDraw.Draw(image)
+        fnt = ImageFont.truetype('LiberationMono-Regular.ttf', 24)
+        d.text((10, 10), "{:05d}".format(self.frame_number), font=fnt, fill=(255, 255, 255, 255))
+        for b in self.balls:
+            d.ellipse([b.x, b.y, b.x + b.r, b.y + b.r], fill=b.color)
+
+def main():
+    balls = Balls()
+
+    for i in range(30):
+        image = Image.new(mode='RGBA', size=settings['img_size'])
+        balls.render(image)
+        name = balls.iter()
+        image.save(name + '.webp', lossless=1, method=6)
+        print ('save: {}'.format(name + '.webp'))
+
+if __name__ == '__main__':
+    main()
