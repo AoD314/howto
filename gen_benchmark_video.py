94df073f3d2f500043a399bc789cc5cbadd3cb8e
Andrey Morozov <morozov.andrey.vmk@gmail.com>
2019-08-30 16:18:31 +0300

    Update gen_benchmark_video.py

--- gen_benchmark_video.py
+++ gen_benchmark_video.py
@@ -3,43 +3,48 @@ import random
 
 settings = {
     'img_size': (1920, 1080),
-    'ratio': 0.1
+    'ratio': 0.025
 }
 
-colors = [
-    (0, 0, 128, 255),
-    (0, 0, 255, 255),
-    (0, 128, 0, 255),
-    (0, 128, 128, 255),
-    (0, 128, 255, 255),
-    (0, 255, 0, 255),
-    (0, 255, 128, 255),
-    (0, 255, 255, 255),
-    (128, 0, 0, 255),
-    (128, 0, 128, 255),
-    (128, 0, 255, 255),
-    (128, 128, 0, 255),
-    (128, 128, 128, 255),
-    (128, 128, 255, 255),
-    (128, 255, 0, 255),
-    (128, 255, 128, 255),
-    (128, 255, 255, 255),
-    (255, 0, 0, 255),
-    (255, 0, 128, 255),
-    (255, 0, 255, 255),
-    (255, 128, 0, 255),
-    (255, 128, 128, 255),
-    (255, 128, 255, 255),
-    (255, 255, 0, 255),
-    (255, 255, 128, 255),
-    (255, 255, 255, 255)
-]
+def get_color():
+    colors = [
+        (0, 0, 128, 255),
+        (0, 0, 255, 255),
+        (0, 128, 0, 255),
+        (0, 128, 128, 255),
+        (0, 128, 255, 255),
+        (0, 255, 0, 255),
+        (0, 255, 128, 255),
+        (0, 255, 255, 255),
+        (128, 0, 0, 255),
+        (128, 0, 128, 255),
+        (128, 0, 255, 255),
+        (128, 128, 0, 255),
+        (128, 128, 128, 255),
+        (128, 128, 255, 255),
+        (128, 255, 0, 255),
+        (128, 255, 128, 255),
+        (128, 255, 255, 255),
+        (255, 0, 0, 255),
+        (255, 0, 128, 255),
+        (255, 0, 255, 255),
+        (255, 128, 0, 255),
+        (255, 128, 128, 255),
+        (255, 128, 255, 255),
+        (255, 255, 0, 255),
+        (255, 255, 128, 255),
+        (255, 255, 255, 255)
+    ]
+
+    i = random.randint(0, len(colors) - 1)
+    return colors[i]
+
 
 
 class Ball:
     def __init__(self, x, y, r, color, border):
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
         self.r = r
         self.color = color
         self.border = border
@@ -49,19 +54,25 @@ class Ball:
             ]
 
     def iter(self, speed=1):
-        self.x += self.dir[0] * speed
-        self.y += self.dir[1] * speed
+        for _ in range(speed):
+            self.x += self.dir[0]
+            self.y += self.dir[1]
 
-        self.dir[0] = -self.dir[0] if self.x - self.r <= 0 else self.dir[0]
-        self.dir[1] = -self.dir[1] if self.y - self.r <= 0 else self.dir[1]
+            self.dir[0] = -self.dir[0] if self.x - self.r <= 0 else self.dir[0]
+            self.dir[1] = -self.dir[1] if self.y - self.r <= 0 else self.dir[1]
 
-        self.dir[0] = -self.dir[0] if self.x + self.r >= self.border[0] else self.dir[0]
-        self.dir[1] = -self.dir[1] if self.y + self.r >= self.border[1] else self.dir[1]
+            self.dir[0] = -self.dir[0] if self.x + self.r >= self.border[0] else self.dir[0]
+            self.dir[1] = -self.dir[1] if self.y + self.r >= self.border[1] else self.dir[1]
+
 
 class Balls:
-    def __init__(self, speed=0.1):
-        self.balls = [Ball(settings['img_size'][0] / 2, settings['img_size'][1] / 2, min(settings['img_size']) * settings['ratio'], colors[-8], settings['img_size'])]
-        self.speed = speed * settings['img_size'][1]
+    def __init__(self, nb=10, speed=0.01):
+        self.balls = []
+        r = min(settings['img_size']) * settings['ratio']
+        for _ in range(nb):
+            self.balls.append(Ball(random.randint(2 * r, settings['img_size'][0] - 2 * r), random.randint(2 * r, settings['img_size'][1] - 2 * r), r, get_color(), settings['img_size']))
+
+        self.speed = int(speed * settings['img_size'][1])
         self.frame_number = 0
 
     def iter(self):
@@ -74,14 +85,14 @@ class Balls:
     def render(self, image):
         d = ImageDraw.Draw(image)
         fnt = ImageFont.truetype('LiberationMono-Regular.ttf', 24)
-        d.text((10, 10), "{:05d}".format(self.frame_number), font=fnt, fill=(255, 255, 255, 255))
         for b in self.balls:
-            d.ellipse([b.x, b.y, b.x + b.r, b.y + b.r], fill=b.color)
+            d.ellipse([b.x - b.r, b.y - b.r, b.x + b.r, b.y + b.r], fill=b.color)
+        d.text((10, 10), "{:05d}".format(self.frame_number), font=fnt, fill=(255, 255, 255, 255))
 
 def main():
-    balls = Balls()
+    balls = Balls(nb=100, speed=0.02)
 
-    N = 600
+    N = 300
 
     for i in range(N):
         image = Image.new(mode='RGB', size=settings['img_size'])
